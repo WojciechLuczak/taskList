7e36916a7b364334c7dfdd9502289f505e9da7fe
Wojciech Łuczak <140091113+WojciechLuczak@users.noreply.github.com>
2023-08-02 21:19:33 +0200

    after corrections from comments

--- js/script.js
+++ js/script.js
@@ -1,34 +1,26 @@
 {
-    const inputField = document.querySelector(".addNewTask__input");
-    const submitButton = document.querySelector(".addNewTask__button");
-
-    submitButton.addEventListener("click", function () {
-        inputField.focus();
-
-    });
-
-    const tasks = [
-
-    ];
-
+    const focus = () => {
+        const submitButton = document.querySelector(".section__button");
+        const inputField = document.querySelector(".section__input");
+        submitButton.addEventListener("click", function () {
+            inputField.focus();
+        });
+    }
+    const tasks = [];
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
-
         render();
     }
-
     const removeTask = (taskIndex) => {
         tasks.splice(taskIndex, 1);
         render();
     }
-
     const toggleTaskDone = (taskIndex) => {
         tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     }
-
     const bindEvents = () => {
         const removeButtons = document.querySelectorAll(".js-remove");
 
@@ -38,74 +30,54 @@
 
             })
         })
-
         const toggleDoneButtons = document.querySelectorAll(".js-done");
-
         toggleDoneButtons.forEach((toggleDoneButton, index) => {
             toggleDoneButton.addEventListener("click", () => {
                 toggleTaskDone(index);
-
             })
         })
     }
-
     const render = () => {
         let htmlString = "";
-
         for (const task of tasks) {
             htmlString += `
-            
-            <li class="taskList__taskRow" ${task.done ? " style=\"text-decoration: line-through\"" : ""} >
-            
-            
-            <div class="taskList__taskRowLeft">
-            <button class="js-done taskList__done">
-            <img class="taskList__iconCheck" src="${task.done ? 'img/checked.png' : 'img/unchecked.png'}" alt="Done Icon">
-            </button>
-            
-            <span class="taskList__individualTask"> ${task.content} </span>
-            </div>
+            <li class="section__taskRow" ${task.done ? " style=\"text-decoration: line-through\"" : ""} >
+                <div class="section__taskRowLeft">
+                    <button class="js-done section__done">
+                        <img class="section__iconCheck" src="${task.done ? 'img/checked.png' : 'img/unchecked.png'}" alt="Done Icon">
+                    </button>
+                
+                    <span class="section__individualTask"> ${task.content} </span>
+                </div>
 
-            <div class="taskList__taskRowRight"><button class="js-remove taskList__remove">
-            <img class="taskList__iconRemove" src="img/remove.png" alt="Remove Icon">
-            </button>
-            </div>
-            
+                <div class="section_taskRowRight">
+                    <button class="js-remove section__remove">
+                        <img class="section__iconRemove" src="img/remove.png" alt="Remove Icon">
+                    </button>
+                </div>
             </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
-
-
-
         bindEvents();
     };
 
-
-
     const onFormSubmit = (event) => {
         event.preventDefault();
-
+        const inputField = document.querySelector(".section__input");
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
         if (newTaskContent === "") {
             return;
         }
-
         addNewTask(newTaskContent);
         inputField.value = "";
-
     }
-
-
     const init = () => {
         render();
-
         const form = document.querySelector(".js-form");
-
         form.addEventListener("submit", onFormSubmit);
+        focus();
     };
-
     init();
 }
